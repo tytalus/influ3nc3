f5bf89cd2ca4acceec3934c8a287d37bd2eb5daa
Mona Lisa <dev@digodat.com>
2018-04-08 19:38:18 -0300

    bigquery test

---
+++ biqueryTest.py
@@ -0,0 +1,17 @@
+# Imports the Google Cloud client library
+from google.cloud import bigquery
+
+# Instantiates a client
+bigquery_client = bigquery.Client()
+
+# The name for the new dataset
+dataset_id = 'my_new_dataset'
+
+# Prepares a reference to the new dataset
+dataset_ref = bigquery_client.dataset(dataset_id)
+dataset = bigquery.Dataset(dataset_ref)
+
+# Creates the new dataset
+dataset = bigquery_client.create_dataset(dataset)
+
+print('Dataset {} created.'.format(dataset.dataset_id))
