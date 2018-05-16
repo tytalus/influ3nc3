0364874ce6e9b20bdad9aed2dc30cc27ca7e38e5
tytalus <elduendemalefico@gmail.com>
2018-05-15 21:21:22 -0300

    python 3 version of parsepages

--- parsePages.py
+++ parsePages.py
@@ -1,6 +1,5 @@
 # coding=utf-8
-import urllib2
-import string
+import requests
 import io
 from bs4 import BeautifulSoup
 import time
@@ -12,14 +11,13 @@ def parseNoticia(noticiaSoup):
     noticia['title'] = article.find('h1', class_ = 'titulo').get_text().strip()
     cuerpo = article.find('section', id = 'cuerpo')
     paragraphs = [p.get_text().strip() for p in cuerpo.find_all('p')]
-    noticia['cuerpo'] = string.join(paragraphs, ' ')
+    noticia['cuerpo'] = ' '.join(paragraphs)
     fileName = './news/' + noticia['title']+'.txt'
     with io.open(fileName, 'w') as file:
         file.write(noticia['cuerpo'])
 
 
 quote_page = 'https://www.lanacion.com.ar/'
-
 request_headers = {
 "Accept-Language": "en-US,en;q=0.5",
 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
@@ -28,12 +26,12 @@ request_headers = {
 "Connection": "keep-alive" 
 }
 
-request = urllib2.Request(quote_page, headers=request_headers)
+#request = urllib.request(quote_page, headers=request_headers)
 # query the website and return the html to the variable 'page'
-page = urllib2.urlopen(request).read()
+result = requests.get(quote_page)
 
 # parse the html using beautiful soup and store in variable 'soup'
-soup = BeautifulSoup(page, 'html.parser')
+soup = BeautifulSoup(result.content, 'html.parser')
 
 # Take out the <div> of name and get its value
 noticias = soup.find_all('article', class_= 'noticia')
@@ -41,10 +39,9 @@ for noticia in noticias:
     try:
         link = noticia.find('a')
         url = link['href'].strip()
-        print url
-        request = urllib2.Request(quote_page + url, headers=request_headers)
-        page = urllib2.urlopen(request).read()
-        noticiaSoup = BeautifulSoup(page, 'html.parser')
+        print(url)
+        result = requests.get(quote_page + url)
+        noticiaSoup = BeautifulSoup(result.content, 'html.parser')
         parseNoticia(noticiaSoup)
     except Exception: 
         pass
